7081898259ca437d444774972a6a3a2664d8b57b
Theo Rozier <contact@theorozier.fr>
2023-02-25 01:43:44 +0100

    Renamed "loginapp" example to "server" because it will contains both loginapp and baseapp

--- wg-toolkit/examples/server.rs
+++ wg-toolkit/examples/server.rs
@@ -33,8 +33,7 @@ fn main() {
     let login_codec = LoginRequestCodec::Server(privkey);
     let mut clients = HashMap::new();
 
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 20016);
-    let mut app = App::new(addr, true).unwrap();
+    let mut app = App::new("0.0.0.0:20016".parse().unwrap(), true).unwrap();
     let mut events = Vec::new();
 
     loop {
