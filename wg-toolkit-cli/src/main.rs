ddf8f2f7cb0cd016a5b3f2455b1aaa6c930ecdf7
Théo Rozier <contact@theorozier.fr>
2024-04-05 22:31:30 +0200

    Reworking CLI

--- wg-toolkit-cli/src/main.rs
+++ wg-toolkit-cli/src/main.rs
@@ -9,64 +9,97 @@
 use std::path::PathBuf;
 use std::process::ExitCode;
 
-use clap::{arg, crate_authors, crate_description, crate_version, value_parser, Command};
+use clap::{Parser, Subcommand};
 
 mod pxml;
 mod res;
 
 
+/// Command line utility for interacting with codecs distributed by Wargaming.net studio.
+/// 
+/// This command line tries to provide UNIX-oriented commands that can be piped together
+/// to make more complex operations.
+#[derive(Debug, Parser)]
+#[command(version, disable_help_subcommand = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Packed XML read and write utilities.
+    Pxml,
+    /// Game resources virtual filesystem access.
+    /// 
+    /// The game resources are split in many directories under the game's resources (res)
+    /// directory, most of resources are actually stored inside huge package files (.pkg).
+    /// This command uses efficient indexing on these packages to efficiently fetch and
+    /// interact with the files, this works with not-packaged files and packaged files at
+    /// the same time.
+    Res {
+        /// Path to the game's resource (res/) directory.
+        res_dir: PathBuf,
+    },
+}
+
+
 fn main() -> ExitCode {
 
-    let matches = Command::new("wgtk")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .disable_help_subcommand(true)
-        .arg_required_else_help(true)
-        .subcommand_required(true)
-        .subcommand(Command::new("pxml")
-            .about("Packed XML read and write utilities")
-            .arg_required_else_help(true)
-            .subcommand_required(true)
-            .subcommand(Command::new("show")
-                .about("Show a deserialized view of a given Packed XML file")
-                .arg(arg!(path: -p --path <PATH> "Path to a specific value to show"))
-                .arg(arg!(xml: -x --xml "Enable XML output style"))
-                .arg(arg!(file: <FILE> "The Packed XML file to show")))
-            .subcommand(Command::new("edit")
-                .about("Edit a terminal value of a given Packed XML file")
-                .arg(arg!(file: <FILE> "The Packed XML file to edit"))
-                .arg(arg!(path: <PATH> "The path to the terminal value to edit"))
-                .arg(arg!(value: <VALUE> "The new value"))))
-        .subcommand(Command::new("res")
-            .about("Resources flatten filesystem utilities")
-            .arg_required_else_help(true)
-            .subcommand_required(true)
-            .arg(arg!(res_dir: <PATH> "Path to the game's res/ directory")
-                .value_parser(value_parser!(PathBuf)))
-            .subcommand(Command::new("ls")
-                .about("List directory contents")
-                .arg(arg!(path: <PATH> "Path to the directory to list, no leading separator (empty to list root)!"))
-                .arg(arg!(recurse: -r --recurse [RECURSION] "Enable recursion listing of directories.")
-                    .value_parser(value_parser!(u16))
-                    .default_missing_value("10000")))
-            .subcommand(Command::new("read")
-                .about("Read a file and write its content on the standard output")
-                .arg(arg!(path: <PATH> "Path to the file to read, no leading separator!"))))
-        .get_matches();
+    let args = Cli::parse();
 
-    let res = match matches.subcommand() {
-        Some(("pxml", matches)) => pxml::cmd_pxml(matches),
-        Some(("res", matches)) => res::cmd_res(matches),
-        _ => unreachable!()
-    };
+    ExitCode::SUCCESS
 
-    if let Err(message) = res {
-        eprintln!("{message}");
-        ExitCode::FAILURE
-    } else {
-        ExitCode::SUCCESS
-    }
+    // let matches = Command::new("wgtk")
+    //     .version(crate_version!())
+    //     .author(crate_authors!())
+    //     .about(crate_description!())
+    //     .disable_help_subcommand(true)
+    //     .arg_required_else_help(true)
+    //     .subcommand_required(true)
+    //     .subcommand(Command::new("pxml")
+    //         .about("Packed XML read and write utilities")
+    //         .arg_required_else_help(true)
+    //         .subcommand_required(true)
+    //         .subcommand(Command::new("show")
+    //             .about("Show a deserialized view of a given Packed XML file")
+    //             .arg(arg!(path: -p --path <PATH> "Path to a specific value to show"))
+    //             .arg(arg!(xml: -x --xml "Enable XML output style"))
+    //             .arg(arg!(file: <FILE> "The Packed XML file to show")))
+    //         .subcommand(Command::new("edit")
+    //             .about("Edit a terminal value of a given Packed XML file")
+    //             .arg(arg!(file: <FILE> "The Packed XML file to edit"))
+    //             .arg(arg!(path: <PATH> "The path to the terminal value to edit"))
+    //             .arg(arg!(value: <VALUE> "The new value."))))
+    //     .subcommand(Command::new("res")
+    //         .about("Resources flatten filesystem utilities.")
+    //         .arg_required_else_help(true)
+    //         .subcommand_required(true)
+    //         .arg(arg!(res_dir: <PATH> "Path to the game's res/ directory.")
+    //             .value_parser(value_parser!(PathBuf)))
+    //         .subcommand(Command::new("ls")
+    //             .about("List directory contents")
+    //             .arg(arg!(path: <PATH> "Path to the directory to list, no leading separator (empty to list root)!"))
+    //             .arg(arg!(recurse: -r --recurse [RECURSION] "Enable recursion listing of directories.")
+    //                 .value_parser(value_parser!(u16))
+    //                 .default_missing_value("10000")))
+    //         .subcommand(Command::new("read")
+    //             .about("Read a file and write its content on the standard output.")
+    //             .arg(arg!(path: <PATH> "Path to the file to read, no leading separator!"))))
+    //     .get_matches();
+
+    // let res = match matches.subcommand() {
+    //     Some(("pxml", matches)) => pxml::cmd_pxml(matches),
+    //     Some(("res", matches)) => res::cmd_res(matches),
+    //     _ => unreachable!()
+    // };
+
+    // if let Err(message) = res {
+    //     eprintln!("{message}");
+    //     ExitCode::FAILURE
+    // } else {
+    //     ExitCode::SUCCESS
+    // }
     
 }
 
